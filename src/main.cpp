74b3f69f136cd80db12861ea6c97fc1b26d40346
ArcosJuan <arcos0juan@gmail.com>
2022-05-21 20:46:17 -0300

    Add base project provided by our teacher.

---
+++ src/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include <time.h>
+#include "filters.h"
+#include <thread>
+#include <unistd.h>
+#include <fstream>      // std::ofstream
+#define ONE_OVER_BILLION 1E-9
+
+
+using namespace std;
+
+int main(int argc , char* argv[]){
+	
+	if(string(argv[1]) == "-help"){
+		cout << "Usage: ./main <filter> <nthreads> <[p1]> <img1> <custom_output> <[p2]> <img2>" << endl;
+		return 0; 
+	}
+	
+	string filter = string(argv[1]);
+	unsigned int n = atoi(argv[2]);
+	float p1 = atof(argv[3]);
+	string img1(argv[4]);
+	string out = string(argv[5]);
+	
+	ppm img(img1);
+	
+	cout << "Applying filters:"<< endl;
+	struct timespec start, stop;    	
+	clock_gettime(CLOCK_REALTIME, &start);
+
+	if (filter == "plain")
+		plain(img, (unsigned char)p1);
+	
+   	clock_gettime(CLOCK_REALTIME, &stop);
+
+	double accum;
+	accum = ( stop.tv_sec - start.tv_sec ) + ( stop.tv_nsec - start.tv_nsec ) * ONE_OVER_BILLION;
+	printf("%lf s\n", accum);
+
+	cout << "Writing image. . ." << endl;
+	img.write(out);	
+	    
+	cout << "DONE!" << endl;
+	return 0;
+}
