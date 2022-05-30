b136f4778417d6168ea3d4d17f89abef8adbf199
ArcosJuan <arcos0juan@gmail.com>
2022-05-30 19:53:01 +0000

    Fix filter to not show white space.

--- src/filters.cpp
+++ src/filters.cpp
@@ -46,25 +46,16 @@ void contrast(ppm& img, float contrast) {
 ppm merge(ppm& img1, ppm& img2, float alpha) {
 	// TODO: manage exceptions  (apha <= 1 && apha >= 0).
 
-	// Create an image as tall as the tallest image and as wide as the widest image.
-	int height = (img1.height>img2.height) ? img1.height : img2.height;
-	int width = (img1.width>img2.width) ? img1.width : img2.width;
+	int height = (img1.height>img2.height) ? img2.height : img1.height;
+	int width = (img1.width>img2.width) ? img2.width : img1.width;
 	ppm out_img(width, height);
 
-	for(int i = 0; i < out_img.height; i++)
+	for(int i = 0; i < out_img.height; i++) {
 		for(int j = 0; j < out_img.width; j++) {
-			if ((img1.height<=i || img1.width<=j) && (img2.height<=i || img2.width<=j))
-				// If none of the images occupy the pixel sets the pixel to white.
-				out_img.setPixel(i,j, pixel{255,255,255});
-			else {
-				// If the images don't overlap in a pixel sets the pixel of the image that occupies the pixel.
-				pixel img1_pixel = (img1.height<=i || img1.width<=j) ?  img2.getPixel(i,j) : img1.getPixel(i,j);
-				pixel img2_pixel = (img2.height<=i || img2.width<=j) ? img1.getPixel(i,j) : img2.getPixel(i,j);
-
-				// (r1*p1 + r2*p2, g1*p1 + g2*p2, b1*p1 + b2*p2).
-				// p1=alpha, p2=1-alpha.
-				out_img.setPixel(i,j, img1_pixel.mult(alpha).addp(img2_pixel.mult(1-alpha)));
-			}
+			// (r1*p1 + r2*p2, g1*p1 + g2*p2, b1*p1 + b2*p2).
+			// p1=alpha, p2=1-alpha.
+			out_img.setPixel(i,j, img1.getPixel(i,j).mult(alpha).addp(img2.getPixel(i,j).mult(1-alpha)));
 		}
+	}
 	return out_img;
 }
