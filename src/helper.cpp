0c2388e5ecc6fdfb2596d95cab6bb6b0bdb4851e
Matimed <mcarazomedley@gmail.com>
2022-06-11 02:58:39 -0300

    Add help info.

--- src/helper.cpp
+++ src/helper.cpp
@@ -9,6 +9,54 @@ using namespace std;
 
 string help(const string option) {
     map<string, string> help;
+    
+    help["main"] =  "IMAGINE - Image Filters \n"
+                    "\nUsage: main [IMG_IN] [IMG_OUT] [OPTION...] \n"
+                    "\nOptions: \n"
+                    "   -h | --help [COMMAND] \n"
+                    "       Show command info \n\n"
+                    "   -f | --filter [FILTER_NAME] [N_THREADS] [ARG...] \n"
+                    "       Applies the defined filter to the input image.  "
+                            "You can apply this command many times and "
+                            "the filters will be executed in the requested order.\n\n"
+                    "   -s | --single-thread\n"
+                    "       Run all desired filters in a single thread.\n\n"
+                     "   -m | --multi-thread [N_THREADS]\n"
+                    "       Run all desired filters in a certain number of threads.\n\n"
+                    "\nFilters:\n"
+                    "   plain [N_THREADS] [HUE]\n"
+                    "   blackwhite [N_THREADS]\n"
+                    "   contrast [N_THREADS] [LEVEL]\n"
+                    "   bright [N_THREADS] [BRIGHT]\n"
+                    "   shades [N_THREADS] [SCALE]\n"
+                    "   merge [N_THREADS] [IMG_2] [ALPHA]\n"
+                    "   frame [N_THREADS] [WIDTH] [HUE]\n"
+                    "   blur [N_THREADS] [LEVEL]\n"
+                    "   zoom [N_THREADS] [LEVEL]\n"
+                    "   edge [N_THREADS]\n"
+                    "\nExamples: \n"
+                    "   main /imgs/motor.ppm /images/output.ppm -m 6 -f contrast 50 -f frame 20 200 \n"
+                    "       Will apply the contrast and frame filters in that order in 6 threads. \n\n"
+                    "   main ./imgs/totoro.ppm /images/output.ppm -f zoom 6 4 -f blackwhite 2 -f bright 1 0.5 \n"
+                    "        Will apply zoom with 6 threads, blackwhite with 2 and bright with one in that order. \n\n";
+
+
+
+    help["filter"] = "Option -f: {-f|--filter} [FILTER] [N_THREADS] [ARG...]";
+    help["single-thread"] = "Option -s: {-s|--single-thread}";
+    help["multi-thread"] = "Option -s: {-m|--multi-thread} [N_THREADS]";
+
+    help["plain"] = "Usage: plain [N_THREADS] [HUE]";
+    help["blackwhite"] = "Usage: blackwhite [N_THREADS]";
+    help["contrast"] = "Usage: contrast [N_THREADS] [LEVEL]";
+    help["bright"] = "Usage: bright [N_THREADS] [BRIGHT]";
+    help["shades"] = "Usage: shades [N_THREADS] [SCALE]";
+    help["merge"] = "Usage: merge [N_THREADS] [IMG_2] [ALPHA]";
+    help["frame"] = "Usage: frame [N_THREADS] [WIDTH] [HUE]";
+    help["blur"] = "Usage: blur [N_THREADS] [LEVEL]";
+    help["zoom"] = "Usage: zoom [N_THREADS] [LEVEL]";
+    help["edge"] = "Usage: edge [N_THREADS]";
+
     return help[option];
 }
 
